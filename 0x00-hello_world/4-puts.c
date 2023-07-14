18788005545f81486853f9421c2e1b80262c1576
etsubet <bethnisolomon@gmailcom>
2023-07-14 06:52:58 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-*main - Entry point
+*main-Entry point
 *
-*Return: Always 0 (Success)
+*Return:Always0(Success)
 */
-int main(void)
-{puts("\"Programming is like building a multilingual puzzle");return (0);}
+intmain(void)
+{puts("\"Programming is like building a multilingual puzzle");return(0);}
