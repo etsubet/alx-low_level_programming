e3caef0a20e0e2a8f516af7e7c00ecb733263b20
etsubet <bethnisolomon@gmailcom>
2023-07-14 06:56:52 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
-#include<stdio.h>
-/**
-*main-Entry point
-*
-*Return:Always0(Success)
-*/
-intmain(void)
-{puts("\"Programming is like building a multilingual puzzle");return(0);}
+#include <stdio.h> 
+/** 
+**main - Entry point 
+* 
+*Return: Always 0 (Success) 
+*/ 
+int main(void) { puts("\"Programming is like building a multilingual puzzle");return (0);}
