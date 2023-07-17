e4951c80168747b179a2fa93c2232c31839ccb66
etsubet <bethnisolomon@gmailcom>
2023-07-17 09:38:19 -0700

    print c4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,50 +1,34 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Description: Print all possible combinations of two two-digit numbers.
- * Numbers should range from 0 to 99.
- * The two numbers should be separated by a space.
- * All numbers should be printed with two digits. 1 should be printed as 01.
- * Combination of numbers must be separated by a comma followed by a space.
- * Combinations of numbers should be printed in ascending order.
- * 00 01 and 01 00 are considered as the same combination.
- * You can only use putchar to print to console.
- * You can only use putchar up to 8 times.
- * You are not allowed to use any variable of type char.
- * Return: 0
+ * main - A program that  prints all possible different
+ * combinations of three digits.
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 {
-int i, j;
-int a, b, c, d;
+int i, j, k;
 
-for (i = 0; i < 100; i++)
-}
-a = i / 10; /* doubles fnum */
-b = i % 10; /* singles fnum */
-
-for (j = 0; j < 100; j++)
+for (i = 0; i < 10; i++)
 {
-c = j / 10; /* doubles snum */
-d = j % 10; /* singles snum */
-
-if (a < c || (a == c && b < d))
+for (j = i + 1; j < 10; j++)
 {
-putchar(a + '0');
-putchar(b + '0');
-putchar(32);
-putchar(c + '0');
-putchar(d + '0');
-
-if (!(a == 9 && b == 8))
+for (k = j + 1; k < 10; k++)
 {
-putchar(44);
-putchar(32);
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+
+if (i < 7)
+{
+putchar(',');
+putchar(' ');
 }
 }
 }
 }
-putchar(10);
+putchar('\n');
 
 return (0);
 }
