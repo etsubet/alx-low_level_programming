dc8ba7a86ff8b389aa82be8f259259a448ea36f5
etsubet <bethnisolomon@gmailcom>
2023-08-08 13:24:20 -0700

    args2

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,52 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 /**
- * main - print the number of arguments passed to the program
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * isInteger - checks if s is an integer
+ * @s: string to check
+ * Return: 0 or 1
  */
-int main(int argc, char *argv[] attribute((unused)))
+int isInteger(const char *s)
 {
-printf("%d\n", argc - 1);
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] < '0' || s[i] > '9')
+return (0);
+i++;
+}
+return (1);
+}
+/**
+ * main - adds positive numbers
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+int i = 0, coinUsed = 0, coin = 0;
+int coins[] = {25, 10, 5, 2, 1};
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+if (isInteger(argv[1]))
+{
+i = atoi(argv[1]);
+while (i > 0 && coin <= 4)
+{
+if (i >= coins[coin])
+{
+i -= coins[coin];
+coinUsed++;
+}
+else
+{
+coin++;
+}
+}
+}
+printf("%i\n", coinUsed);
 return (0);
 }
